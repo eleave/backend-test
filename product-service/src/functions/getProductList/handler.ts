61d62c8bf049a281a4169dcee3b1be06acf63ace
alex <alexeleave@gmail.com>
2022-07-31 12:50:10 +0300

    functions: getProductById and getProductList intergrated  with db

--- product-service/src/functions/getProductList/handler.ts
+++ product-service/src/functions/getProductList/handler.ts
@@ -1,10 +1,25 @@
 import { formatJSONResponse } from "@libs/api-gateway";
 import { middyfy } from "@libs/lambda";
-
-import PRODUCTS from "../../data/products.json";
+import { Client } from "pg";
+import dbConfig from "@libs/db/config";
 
 const getProductList = async () => {
-  return formatJSONResponse(PRODUCTS);
+  const client = new Client(dbConfig);
+
+  try {
+    await client.connect();
+    const result = await client.query(`
+      SELECT id, name, description, price, count FROM products
+      LEFT JOIN stocks ON products.id = stocks.product_id
+    `);
+
+    return formatJSONResponse(result.rows);
+  } catch (error) {
+    console.error(error);
+    return error;
+  } finally {
+    await client.end();
+  }
 };
 
 export const getProductListHandler = middyfy(getProductList);
